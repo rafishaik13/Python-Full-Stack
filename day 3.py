f561813069db81e73760ab93400168a9c71ffc8f
shaik mahammad rafi <mahammadrafishaik313@gmail.com>
2026-08-06 21:15:14 +0530

    added day3 python notes

---
+++ day 3.py
@@ -0,0 +1,24 @@
+SWAPPING TWO NUMBERS
+
+a=10
+b=20
+temp=a
+a=b
+b=temp
+print(a,b) 
+
+a=25
+b=20
+a,b=b,a
+print(a,b)
+
+a=89
+b=76
+a=a+b
+b=a-b
+a=a-b
+print(a,b)
+
+Data types:
+data types are used to define type of object  and what kind of operations performed on it
+* python automatically identifies type of object or value when you assign object to variable
